19aceaaafc17b781a5a5315749975d3bb7fa1095
Betsy Bayliss <betsy.bayliss@multiverse.io>
2022-11-04 10:28:13 -0700

    adds ackend demo rd 2

--- REST/rest-demo/index.js
+++ REST/rest-demo/index.js
@@ -7,94 +7,25 @@ const { Op } = require('sequelize');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-/**
- * middleware that gets dogs that match query strings
- * not as slow as a dogs.findAll and a dogs.filter
- * not reliant on any information passed in by user
- * get req.query object, use that object in the findAll
- */
-
-const matchingDogs = async (req, res, next) => {
-  const queryObject = req.query;
-  let dogs;
-  //console.log(queryObject);
+app.get('/dogs', async (req, res, next) => {
   try {
-    if (Object.keys(queryObject).length !== 0) {
-      dogs = await Dog.findAll({where: queryObject});
-    } else {
-      dogs = await Dog.findAll();
+    const where = {};
+    
+    for(const key of [name, breed, color, description]) {
+      if(req.query[key]) {
+        where[key] = {
+          [Op.like]: `%${req.query[key]}%` // searches within the string and finds similar matches
+        }
+      }
     }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-  
-  
-  res.locals.dogs = dogs;
-  next();
-}
 
-/**
- * this function which resembles the curriculum solutions requires information about the query strings passed in to run correctly
- * depending on client-side input != restful
- * 
- * 
- */
-
-// app.get('/dogs/', async (req, res, next) => {
-
-//   if(Object.keys(req.query).length !== 0) {
-//     const {color} = req.query 
-//     const dogs = await Dog.findAll({where: {color}})
-//     res.send(dogs)
-//   } else {
-//     const dogs = await Dog.findAll()
-//     res.send(dogs)
-//   }
-
-// });
-
-/**
- * the way I initially set up this endpoint only depends on the models
- * it isn't DRY, those if statements are repetitive and if there are a huge amount of columns, you'd need a lot of them
- * also, if there is a huge amount od data in your db, you have to .findAll AND .filter, which could be quite slow
- * 
- * 
- */
-
-
-app.get('/dogs/', matchingDogs, async (req, res, next) => {
-  try {
-    // const dogs = await Dog.findAll();
-
-    // const { name, breed, color, description } = req.query;
-
-    // const matchingDogs = dogs.filter(dog => {
-    //   if (name && !(dog.name === name)) {
-    //     return;
-    //   };
-    //   if (breed && !(dog.breed === breed)) {
-    //     return;
-    //   };
-    //   if (color && !(dog.color === color)) {
-    //     return;
-    //   };
-    //   if (description && !(dog.description === description)) {
-    //     return;
-    //   };
-
-    //   return dog;
-    // });
-
-    res.send(res.locals.dogs);
+    let dogs = await Dog.findAll({where});
+    res.send(dogs);
   } 
   catch (error) {
     next(error)
   }
 });
-
-
-
 
 const { PORT = 4000 } = process.env;
 
